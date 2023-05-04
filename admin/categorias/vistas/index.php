0e5cee265c3aa4fa387a42fa064a3adab00bdf5f
Diego Palacio <diegopalacio@politecnicodecolombia.edu.co>
2023-05-04 07:20:50 -0500

    Cambio boton

--- admin/categorias/vistas/index.php
+++ admin/categorias/vistas/index.php
@@ -44,8 +44,7 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-primary">Registrar Categoría</button>
                     </div>
                 </div>
             </div>
